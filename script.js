f37d9c536aaa0f978d1895252254ba22be80c734
Sephy37 <146596317+Sephy37@users.noreply.github.com>
2023-10-08 12:55:48 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function check() {
+    let num = document.getElementById("num_nums").value;
+
+    if (num === "4") {
+        alert("4 WAS ENTERED!");
+    }else{
+        console.log("Something else was entered!")
+    }
+}
+
+
+
+/*let userInput = document.getElementById("num_nums");
+let button = document.getElementById("btn1");
+
+
+function check(){
+    let num = userInput.value;
+    if(num === "4"){
+        alert("4 WAS ENTERED!")
+    }else{
+        console.log("Something else was entered!")
+    }
+}
+
+button.addEventListener("click",function(){
+    check()
+},false) */
